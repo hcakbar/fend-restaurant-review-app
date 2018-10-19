42c480789bef363bc885496f15d0daa55dea72b5
akbar <hc.akbar@gmail.com>
2018-10-19 12:21:28 -0400

    updating readme.md

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
      * Change this to restaurants.json file location on your server.
      */
     static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
+        const port = 8000 // Change this to your server port
         return `http://localhost:${port}/data/restaurants.json`;
     }
 
@@ -158,11 +158,11 @@ class DBHelper {
      */
     static mapMarkerForRestaurant(restaurant, map) {
         // https://leafletjs.com/reference-1.3.0.html#marker
-        const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-            {title: restaurant.name,
-                alt: restaurant.name,
-                url: DBHelper.urlForRestaurant(restaurant)
-            })
+        const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng], {
+            title: restaurant.name,
+            alt: restaurant.name,
+            url: DBHelper.urlForRestaurant(restaurant)
+        })
         marker.addTo(newMap);
         return marker;
     }
